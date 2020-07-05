bf5c156dec3839008470ebffb2747435b498419f
FuocomanSap <45140267+FuocomanSap@users.noreply.github.com>
2020-07-05 22:35:48 +0200

    reboot added

--- update.sh
+++ update.sh
@@ -7,4 +7,6 @@ mkdir /tmp/updt
 cd /tmp/updt
 git clone https://github.com/FuocomanSap/P4wnP1_aloa_oled_menu.git
 cd P4wnP1_aloa_oled_menu/
-/bin/bash install.sh &
+#/bin/bash install.sh & #the best version, but freeze the rasp
+/bin/bash install.sh
+reboot
